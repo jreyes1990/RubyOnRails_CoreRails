8f47590ca03c2ca52c79ce89888596469ac7d2cf
jreyes1990 <jarl1990mt@gmail.com>
2023-01-03 23:24:57 -0600

    Mi segunda configuracion al core

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,9 +18,9 @@ require("channels")
 
 import "startbootstrap-sb-admin-2/vendor/jquery/jquery";
 import "@popperjs/core/dist/umd/popper";
-import "startbootstrap-sb-admin-2/js/sb-admin-2";
 import "startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle";
 import "startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing";
+//import "startbootstrap-sb-admin-2/js/sb-admin-2";
 import "startbootstrap-sb-admin-2/vendor/fontawesome-free/js/all";
 
 import "datatables.net/js/jquery.dataTables";
@@ -35,6 +35,9 @@ import "datatables.net-buttons/js/buttons.colVis";
 import "datatables.net-buttons/js/buttons.html5";
 import "datatables.net-buttons/js/buttons.print";
 
+import "moment/dist/moment";
+import "moment/dist/locale/es";
+
 let pdfMake = require("pdfmake/build/pdfmake");
 let pdfFonts = require("pdfmake/build/vfs_fonts");
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
@@ -42,6 +45,79 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 import "controllers"
 
 document.addEventListener("turbolinks:load", () => {
+  /* *******************************************************
+   * Para controlar el sidebar en posición cerrado o abierto
+   * ******************************************************** */
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+    if ($(".sidebar").hasClass("toggled")) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+  });
+
+  $(window).resize(function() {
+    // Cierre cualquier acordeón de menú abierto cuando la ventana cambie de tamaño a continuación 768px
+    if ($(window).width() < 768) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+
+    // Cierre cualquier acordeón de menú abierto cuando la ventana cambie de tamaño a continuación 480px
+    if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+      $("body").addClass("sidebar-toggled");
+      $(".sidebar").addClass("toggled");
+      $('.sidebar .collapse').collapse('hide');
+    };
+  });
+  /* *******************************************************
+   * Fin para controlar el sidebar en posición cerrado o abierto
+   * ******************************************************** */
+
+  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+    if ($(window).width() > 768) {
+      var e0 = e.originalEvent,
+          delta = e0.wheelDelta || -e0.detail;
+      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+      e.preventDefault();
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).on('scroll', function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  $("#topBtn").click(function () {
+    $("html ,body").animate(
+        {
+          scrollTop: 0,
+        },
+        800
+    );
+  });
+
+  // Smooth scrolling using jQuery easing
+  $(document).on('click', 'a.scroll-to-top', function(e) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top)
+    }, 1000, 'easeInOutExpo');
+    e.preventDefault();
+  });
+
+  //control de los tiempos de los flash
+  $(".alert")
+      .fadeTo(4000, 500)
+      .slideUp(500, function () {
+        $(".alert").slideUp(4000);
+      });
+
   const originalWebSocketClose = WebSocket.prototype.close
   WebSocket.prototype.close = function () {
     if (this.readyState != WebSocket.CONNECTING) {
