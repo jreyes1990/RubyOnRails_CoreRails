87f476136dfcca35e9f67de3ecc64cd710064293
jreyes1990 <jarl1990mt@gmail.com>
2023-01-09 23:26:19 -0600

    Mi septima configuracion al core

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -93,6 +93,187 @@ document.addEventListener("turbolinks:load", () => {
             originalWebSocketClose.apply(this, arguments)
         }
     }
+
+    // Configuracion para activar select2
+    $(".select2").select2({
+        theme: "bootstrap4",
+        language: "es-GT",
+        width: "100%",
+    });
+
+    /* *******************************************************
+   * Configuracion para dataTables
+   * *********************************************************/
+    var espaniol = {
+        sEmptyTable: "No has datos disponibles en la tabla.",
+        sLengthMenu: "Mostrar _MENU_ Entradas",
+        //"LengthMenu": [[5,10,15,20,25,50,-1],[5,10,15,20,25,50,"Todos"]],
+        //"iDisplayLength": 5,
+        sZeroRecords: "No se encontraron resultados",
+        info: "Mostrando _END_ registros, de _TOTAL_ registros",
+        sInfoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+        sInfoPostFix: "(Actualizados)",
+        sProcessing: "Procesando...",
+        sSearch: "",
+        oPaginate: {
+            sFirst: "Primero",
+            sLast: "Último",
+            sNext: "Siguiente",
+            sPrevious: "Anterior",
+        },
+        aria: {
+            sSortAscending: "Ordenación Ascendente",
+            sSortDescending: "Ordenación Descendente",
+        },
+    };
+
+    var var_dom =
+        "" +
+        "<'row'<'col-sm-3'l><'col-sm-6 text-center'B><'col-sm-3'f>>" +
+        "<'row'<'col-sm-12'tr>>" +
+        "<'row'<'col-sm-5'i><'col-sm-3 text-center'p><'col-sm-4'>>";
+
+    $(document).ajaxSend(function (e, xhr, options) {
+        var token = $("meta[name='csrf-token']").attr("content");
+        xhr.setRequestHeader("X-CSRF-Token", token);
+    });
+
+    $("#datatable").DataTable({
+        fixedHeader: true,
+        stateSave: true,
+        stateDuration: 1200,
+        responsive: "true",
+        dom: var_dom,
+        language: espaniol,
+        lengthChange: true,
+        select: true,
+        order: [0, "desc"],
+        buttons: [
+            {
+                text: 'CSV <i class="fas fa-file-csv"></i> ',
+                extend: "csvHtml5",
+                fieldSeparator: "\t",
+                extension: ".csv",
+                titleAttr: "Si desea exportar el archivo, Dar click en CSV",
+                className: "btn btn-outline-warning",
+            },
+            {
+                text: 'EXCEL <i class="fas fa-file-excel"></i> ',
+                extend: "csvHtml5",
+                fieldSeparator: "\t",
+                extension: ".xlsx",
+                titleAttr: "Si desea exportar el archivo, Dar click en EXCEL",
+                className: "btn btn-outline-success",
+                messageTop: "La exportación excel, se ha realizado correctamente",
+            },
+            {
+                text: 'PDF <i class="fas fa-file-pdf"></i> ',
+                extend: "pdfHtml5",
+                titleAttr: "Si desea exportar el archivo, Dar click en PDF",
+                className: "btn btn-outline-danger",
+                messageTop: "PDF created by PDFMake with Buttons for DataTables.",
+            },
+            {
+                text: 'PRINT <i class="fas fa-print"></i> ',
+                extend: "print",
+                titleAttr: "Si desea imprimr, Dar click en PRINT",
+                className: "btn btn-outline-info",
+                exportOptions: {
+                    columns: ":visible",
+                },
+            },
+            {
+                text: "JSON",
+                titleAttr: "Si desea exportar el archivo, Dar click en JSON",
+                className: "btn btn-outline-primary",
+                action: function (e, dt, button, config) {
+                    var data = dt.buttons.exportData();
+
+                    $.fn.dataTable.fileSave(
+                        new Blob([JSON.stringify(data)]),
+                        "Export.json"
+                    );
+                },
+            },
+        ],
+        pagingType: "full_numbers",
+    });
+
+    $("#opcion-ca-datatable").DataTable({
+        fixedHeader: true,
+        stateSave: true,
+        stateDuration: 1200,
+        responsive: "true",
+        dom: var_dom,
+        language: espaniol,
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: $("#opcion-ca-datatable").data("source"),
+        },
+        order: [0, "desc"],
+        buttons: [
+            {
+                text: 'CSV <i class="fas fa-file-csv"></i> ',
+                extend: "csvHtml5",
+                fieldSeparator: "\t",
+                extension: ".csv",
+                titleAttr: "Si desea exportar el archivo, Dar click en CSV",
+                className: "btn btn-outline-warning",
+            },
+            {
+                text: 'EXCEL <i class="fas fa-file-excel"></i> ',
+                extend: "csvHtml5",
+                fieldSeparator: "\t",
+                extension: ".xlsx",
+                titleAttr: "Si desea exportar el archivo, Dar click en EXCEL",
+                className: "btn btn-outline-success",
+                messageTop: "La exportación excel, se ha realizado correctamente",
+            },
+            {
+                text: 'PDF <i class="fas fa-file-pdf"></i> ',
+                extend: "pdfHtml5",
+                titleAttr: "Si desea exportar el archivo, Dar click en PDF",
+                className: "btn btn-outline-danger",
+                messageTop: "PDF created by PDFMake with Buttons for DataTables.",
+            },
+            {
+                text: 'PRINT <i class="fas fa-print"></i> ',
+                extend: "print",
+                titleAttr: "Si desea imprimr, Dar click en PRINT",
+                className: "btn btn-outline-info",
+                exportOptions: {
+                    columns: ":visible",
+                },
+            },
+            {
+                text: "JSON",
+                titleAttr: "Si desea exportar el archivo, Dar click en JSON",
+                className: "btn btn-outline-primary",
+                action: function (e, dt, button, config) {
+                    var data = dt.buttons.exportData();
+
+                    $.fn.dataTable.fileSave(
+                        new Blob([JSON.stringify(data)]),
+                        "Export.json"
+                    );
+                },
+            },
+        ],
+        pagingType: "full_numbers",
+        columns: [
+            { data: "id" },
+            { data: "opcion" },
+            { data: "componente" },
+            { data: "atributo" },
+            { data: "descripcion" },
+            { data: "estado" },
+            { data: "editar" },
+            { data: "inactivar" },
+        ],
+    });
+
 });
 
 
