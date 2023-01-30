ab9ad41cde504a2cdb21e896b95136ab06e6a666
jreyes1990 <jarl1990mt@gmail.com>
2023-01-29 22:18:57 -0600

    29-01-2023 22:20 PM, Mejoras de configuracion de correos electronicos.

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,8 @@ module CoreRails
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
-    #config.action_mailer.raise_delivery_errors = true
     config.action_mailer.default :charset => "utf-8"
 
     ActionMailer::Base.smtp_settings = {
