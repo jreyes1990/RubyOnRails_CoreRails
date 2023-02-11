85999efb9b6dee8f2fd8ee32ad6d7de1f84a56e2
jreyes1990 <jarl1990mt@gmail.com>
2023-02-11 00:22:15 -0600

    11-02-2023 00:20 AM, Se agrego los metodos de permisos.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,48 @@
 module ApplicationHelper
+  include Permisos
+
+  def is_active_action(controller)
+    if params[:controller] == controller
+      "collapse-item active"
+    else
+      "collapse-item"
+    end
+  end
+  
+  # METODOS DE ACTIVACION EN SIDEBAR - INICIO
+  def is_home_active
+    if params[:controller] == "home"
+      "nav-item active"
+    else
+      "nav-item"
+    end
+  end
+
+  # METODOS DE ACTIVACION EN SIDEBAR - ICONOS
+  def is_active_controller_icono_titulo
+    @controladores = ["font_awesomes"]
+    if @controladores.include? params[:controller]
+      "nav-item active"
+    else
+      "nav-item"
+    end
+  end
+
+  def is_active_controller_icono
+    @controladores = ["font_awesomes"]
+    if @controladores.include? params[:controller]
+      "nav-link"
+    else
+      "nav-link collapsed"
+    end
+  end
+
+  def is_active_option_controller_icono
+    @controladores = ["font_awesomes"]
+    if @controladores.include? params[:controller]
+      "collapse show"
+    else
+      "collapse"
+    end
+  end
 end
