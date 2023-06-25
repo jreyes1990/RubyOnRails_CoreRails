9c541df36bdbd5ec014df440f906c55e34db5871
jreyes1990 <jarl1990mt@gmail.com>
2023-06-24 23:09:43 -0600

    24-06-2023 23:05, Mejoras del modulo Personas Areas.

--- app/controllers/personas_areas_controller.rb
+++ app/controllers/personas_areas_controller.rb
@@ -3,8 +3,32 @@ class PersonasAreasController < ApplicationController
 
   # GET /personas_areas or /personas_areas.json
   def index
-    @personas_areas = PersonasAreaView.order(id: :DESC)
+    respond_to do |format|    
+      format.html
+      format.json { render json: PersonasAreaDatatable.new(params, view_context: view_context) }
+    end
   end
+
+  def search_empresa_persona
+    parametro = params[:empresa_usuario_area_params]
+      
+    @empresa =  Empresa.where("(upper(id|| ' ' ||nombre) like upper('%#{parametro}%')) and estado = 'A' ").limit(50).distinct
+
+    respond_to do |format|
+      format.json { render json: @empresa.map { |p| { valor_id: p.id, valor_text: p.informacion_empresa } } }
+    end   
+  end 
+
+  def search_area_persona
+    parametro = params[:empresa_usuario_area_params]
+      
+    @empresa =  Area.joins("inner join empresas on (areas.empresa_id = empresas.id)")
+                    .where("areas.empresa_id = #{parametro} and areas.estado = 'A'").limit(50).distinct
+
+    respond_to do |format|
+      format.json { render json: @empresa.map { |p| { valor_id: p.id, valor_text: p.area_con_codigo } } }
+    end   
+  end 
 
   # GET /personas_areas/1 or /personas_areas/1.json
   def show
@@ -27,10 +51,10 @@ class PersonasAreasController < ApplicationController
 
     respond_to do |format|
       if @personas_area.save
-        format.html { redirect_to personas_areas_url, notice: "Personas area was successfully created." }
+        format.html { redirect_to personas_areas_url, notice: "La Asignación Usuario-Área <strong >#{@personas_area.persona.nombre.capitalize}</strong> - <strong style='color: #{@personas_area.area.codigo_hex}'>#{@personas_area.area.nombre.capitalize}</strong> se ha creado correctamente.".html_safe }
         format.json { render :show, status: :created, location: @personas_area }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity, alert: "Ocurrio un error al crear la Asignación Usuario-Área, Verifique!!.." }
         format.json { render json: @personas_area.errors, status: :unprocessable_entity }
       end
     end
@@ -42,10 +66,10 @@ class PersonasAreasController < ApplicationController
 
     respond_to do |format|
       if @personas_area.update(personas_area_params)
-        format.html { redirect_to personas_areas_url, notice: "Personas area was successfully updated." }
+        format.html { redirect_to personas_areas_url, notice: "La Asignación Usuario-Área <strong >#{@personas_area.persona.nombre.capitalize}</strong> - <strong style='color: #{@personas_area.area.codigo_hex}'>#{@personas_area.area.nombre.capitalize}</strong> se ha actualizado correctamente.".html_safe }
         format.json { render :show, status: :ok, location: @personas_area }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit, status: :unprocessable_entity, alert: "Ocurrio un error al actualizar la Asignación Usuario-Área, Verifique!!.." }
         format.json { render json: @personas_area.errors, status: :unprocessable_entity }
       end
     end
@@ -56,7 +80,7 @@ class PersonasAreasController < ApplicationController
     @personas_area.destroy
 
     respond_to do |format|
-      format.html { redirect_to personas_areas_url, notice: "La Persona-Area <strong >#{@personas_area.persona.nombre}</strong> - <strong style='color: #{@personas_area.area.codigo_hex}'>#{@personas_area.area.nombre}</strong> ha sido Eliminado.".html_safe }
+      format.html { redirect_to personas_areas_url, notice: "La Asignación Usuario-Área <strong >#{@personas_area.persona.nombre}</strong> - <strong style='color: #{@personas_area.area.codigo_hex}'>#{@personas_area.area.nombre}</strong> ha sido Eliminado.".html_safe }
       format.json { head :no_content }
     end
   end
@@ -68,7 +92,7 @@ class PersonasAreasController < ApplicationController
 
     respond_to do |format|
       if @personas_area.save
-        format.html { redirect_to personas_areas_url, notice: "La Persona-Area <strong >#{@personas_area.persona.nombre}</strong> - <strong style='color: #{@personas_area.area.codigo_hex}'>#{@personas_area.area.nombre}</strong> ha sido Inactivado.".html_safe }
+        format.html { redirect_to personas_areas_url, notice: "La Asignación Usuario-Área <strong >#{@personas_area.persona.nombre}</strong> - <strong style='color: #{@personas_area.area.codigo_hex}'>#{@personas_area.area.nombre}</strong> ha sido Inactivado.".html_safe }
         format.json { render :show, status: :created, location: @personas_area }
       else
         format.html { redirect_to personas_areas_url, alert: "Ocurrio un error al inactivar Persona-Area, Verifique!!.." }
@@ -84,7 +108,7 @@ class PersonasAreasController < ApplicationController
 
     respond_to do |format|
       if @personas_area.save
-        format.html { redirect_to personas_areas_url, notice: "La Persona-Area <strong >#{@personas_area.persona.nombre}</strong> - <strong style='color: #{@personas_area.area.codigo_hex}'>#{@personas_area.area.nombre}</strong> ha sido Activado.".html_safe }
+        format.html { redirect_to personas_areas_url, notice: "La Asignación Usuario-Área <strong >#{@personas_area.persona.nombre}</strong> - <strong style='color: #{@personas_area.area.codigo_hex}'>#{@personas_area.area.nombre}</strong> ha sido Activado.".html_safe }
         format.json { render :show, status: :created, location: @personas_area }
       else
         format.html { redirect_to personas_areas_url, alert: "Ocurrio un error al activar Persona-Area, Verifique!!.." }
@@ -101,6 +125,6 @@ class PersonasAreasController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def personas_area_params
-      params.require(:personas_area).permit(:descripcion, :user_created_id, :user_updated_id, :estado, :persona_id, :area_id)
+      params.require(:personas_area).permit(:descripcion, :user_created_id, :user_updated_id, :estado, :persona_id, :area_id, :rol_id)
     end
 end
---
+++ app/datatables/personas_area_datatable.rb
@@ -0,0 +1,136 @@
+class PersonasAreaDatatable < AjaxDatatablesRails::ActiveRecord
+  extend Forwardable
+
+  #Definición de los Helpers de la vista
+  def_delegator :@view, :link_to
+  #def_delegator :@view, :tiene_permiso
+  def_delegator :@view, :edit_personas_area_path
+  def_delegator :@view, :personas_area_path
+  def_delegator :@view, :inactivar_personas_area_path
+  def_delegator :@view, :activar_personas_area_path
+
+  def initialize(params, opts = {})
+    @view = opts[:view_context]
+    super
+  end
+
+  def view_columns
+    @view_columns ||= {
+      id: { source: "PersonasAreaView.id", cond: :eq },
+      nombre_empresa: { source: "PersonasAreaView.nombre_empresa", cond: :like },
+      nombre_area: { source: "PersonasAreaView.nombre_area", cond: :like },
+      nombre_usuario: { source: "PersonasAreaView.nombre_usuario", cond: :like },
+      email_usuario: { source: "PersonasAreaView.email_usuario", cond: :like },
+      nombre_rol: { source: "PersonasAreaView.nombre_rol", cond: :like },
+      estado: { source: "PersonasAreaView.estado", cond: :like },
+      opciones: { source: "", searchable: false, orderable: false},
+      inactivar: { source: "", searchable: false, orderable: false}
+    }
+  end
+
+  def data
+    records.map do |record|
+      {
+        id: record.id,
+        nombre_empresa: estilo_codigo_hex_empresa_area(record),
+        nombre_area: record.nombre_area,
+        nombre_usuario: record.nombre_usuario,
+        email_usuario: record.email_usuario,
+        nombre_rol: estilo_codigo_hex_rol(record),
+        estado: format_estado(record),
+        opciones: show_btn_opcion(record),
+        inactivar: show_btn_inactivar(record)
+      }
+    end
+  end
+
+  def get_raw_records
+    PersonasAreaView.order(nombre_empresa: :DESC, nombre_area: :DESC, nombre_rol: :ASC, id: :DESC)
+  end
+
+  def show_btn_opcion(record)
+    btnOpcion = nil
+    #if tiene_permiso("BOTON EDITAR REGISTRO", "VER") || tiene_permiso("BOTON ELIMINAR REGISTRO", "VER")
+      if record.estado == 'A'
+        #if tiene_permiso("BOTON EDITAR REGISTRO", "VER")
+          btnOpcion =  link_to("<i class='fas fa-edit'></i>".html_safe, edit_personas_area_path(record), class: "btn btn-outline-info btn-sm rounded-circle") 
+        #else
+        #  btnOpcion = ""
+        #end
+      else
+        #if tiene_permiso("BOTON ELIMINAR REGISTRO", "VER")
+          btnOpcion =  link_to("<i class='fas fa-trash-alt'></i>".html_safe, personas_area_path(record.id), class: "btn btn-outline-danger btn-sm rounded-circle", method: :delete, data: {controller: 'sweetalert', action: 'click->sweetalert#btnInactivar',
+                                                                                                                                                                                  sweetalert_confirm_alert_value: "Eliminar",
+                                                                                                                                                                                  sweetalert_cancel_alert_value: "Cancelado",
+                                                                                                                                                                                  sweetalert_confirm_title_value: 'Esta seguro de eliminar <strong style="color: #1d71b9; font-weight: bold;"><p>'+"#{record.nombre_area}-#{record.nombre_usuario}?"+'</p></strong>',
+                                                                                                                                                                                  sweetalert_confirm_btn_value: '<i class="fas fa-check-circle"></i> <strong>Si, Eliminar</strong>',
+                                                                                                                                                                                  sweetalert_cancel_btn_value: '<i class="fas fa-times-circle"></i> <strong>No, Cancelar</strong>',
+                                                                                                                                                                                  sweetalert_cancel_title_value: 'Se ha cancelado la aliminación de <strong>'+"#{record.nombre_area}-#{record.nombre_usuario}"+'</strong>' }) 
+        #else
+        #  btnOpcion = ""
+        #end
+      end
+    #else
+    #  btnOpcion = ""
+    #end
+    return btnOpcion
+  end
+  
+  def show_btn_inactivar(record)
+    btnInactivar = nil
+    #if tiene_permiso("BOTON INACTIVAR REGISTRO", "VER")
+    if record.estado == 'A'
+      btnInactivar = link_to("<i class='fas fa-toggle-on' style='transform: rotate(360deg);'></i>".html_safe, inactivar_personas_area_path(:id => record.id), class: "btn btn-outline-danger btn-sm rounded-circle", data: {controller: 'sweetalert', action: 'click->sweetalert#btnInactivar',
+                                                                                                                                                                                                                    sweetalert_confirm_alert_value: "Inactivar",
+                                                                                                                                                                                                                    sweetalert_cancel_alert_value: "Cancelado",
+                                                                                                                                                                                                                    sweetalert_confirm_title_value: 'Esta seguro de inactivar <strong style="color: #1d71b9; font-weight: bold;"><p>'+"#{record.nombre_area}-#{record.nombre_usuario}?"+'</p></strong>',
+                                                                                                                                                                                                                    sweetalert_confirm_btn_value: '<i class="fas fa-check-circle"></i> <strong>Si, Inactivar</strong>',
+                                                                                                                                                                                                                    sweetalert_cancel_btn_value: '<i class="fas fa-times-circle"></i> <strong>No, Cancelar</strong>',
+                                                                                                                                                                                                                    sweetalert_cancel_title_value: 'Se ha cancelado la inactivación de <strong>'+"#{record.nombre_area}-#{record.nombre_usuario}"+'</strong>' }) 
+    else
+      btnInactivar = link_to("<i class='fas fa-toggle-on' style='transform: rotate(180deg);'></i>".html_safe, activar_personas_area_path(:id => record.id), class: "btn btn-outline-success btn-sm rounded-circle", data: {controller: 'sweetalert', action: 'click->sweetalert#btnInactivar',
+                                                                                                                                                                                                                  sweetalert_confirm_alert_value: "Activar",
+                                                                                                                                                                                                                  sweetalert_cancel_alert_value: "Cancelado",
+                                                                                                                                                                                                                  sweetalert_confirm_title_value: 'Esta seguro de activar <strong style="color: #1d71b9; font-weight: bold;"><p>'+"#{record.nombre_area}-#{record.nombre_usuario}?"+'</p></strong>',
+                                                                                                                                                                                                                  sweetalert_confirm_btn_value: '<i class="fas fa-check-circle"></i> <strong>Si, Activar</strong>',
+                                                                                                                                                                                                                  sweetalert_cancel_btn_value: '<i class="fas fa-times-circle"></i> <strong>No, Cancelar</strong>',
+                                                                                                                                                                                                                  sweetalert_cancel_title_value: 'Se ha cancelado la activación de <strong>'+"#{record.nombre_area}-#{record.nombre_usuario}"+'</strong>' }) 
+    end
+    #else
+    #  btnInactivar = ""
+    #end
+    return btnInactivar
+  end
+
+  def format_estado(record)
+    if record.estado == 'A'
+      badge_estado = "badge badge-success"
+      nombre_estado = "Activo"
+    else
+      badge_estado = "badge badge-danger"
+      nombre_estado = "Inactivo"
+    end
+
+    return "<div class='text-center'><span class='#{badge_estado}'>#{nombre_estado}</span></div>".html_safe
+  end
+
+  def estilo_codigo_hex_empresa_area(record)
+    @color_empresa = Empresa.where(id: record.empresa_id).first
+
+    if !@color_empresa.blank?
+      color_hex = @color_empresa.codigo_hex
+    else
+      color_hex = "#F000"
+    end
+
+    return "<div class='text-center'><strong style='color: #{color_hex};'>#{record.nombre_empresa.upcase}</strong> - <strong style='color: #{record.codigo_hex_area};'>#{record.nombre_area.upcase}</strong></div>".html_safe
+  end
+
+  def estilo_codigo_hex_rol(record)
+    @color_rol = Rol.where(id: record.rol_id).first
+
+    if !@color_rol.blank?
+      return "<div style='color: #{@color_rol.codigo_hex};'>#{record.nombre_rol.upcase}</div>".html_safe
+    end
+  end
+end
