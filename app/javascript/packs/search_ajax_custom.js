9bfe63166ecfc01ad432dcf4d0ee0c99a1f24714
jreyes1990 <jarl1990mt@gmail.com>
2023-07-12 23:47:04 -0600

    12-07-2023 23:40 PM, Reestructuracion de codigo JavaScript.

--- app/javascript/packs/search_ajax_custom.js
+++ app/javascript/packs/search_ajax_custom.js
@@ -50,4 +50,26 @@ document.addEventListener("turbolinks:load", () => {
     });
   });
 
+  // BUSCADOR OPCIONES, EN EL MODULO MENU ROL
+  $('#codigo_menu').on('select2:select', function (e) {
+    $.ajax({
+      url: $('.opciones').data('endpoint'),
+      type: 'GET',
+      dataType: "json",
+      data: {
+          opcion_id_param: e.params.data.id
+      },
+      success: function (data) {
+        $("#codigo_opcion").empty();
+
+        var json = data;
+        $("#codigo_opcion").append("<option value='" + 0 + "'>Seleccione una Opción</option>");
+        for (var i of json) {
+          $("#codigo_opcion").append("<option value='" + i.opcion_id + "'>" + i.descripcion + "</option>");
+          console.log(`Mostrando valores ${i.opcion_id}`)
+        }
+      }
+    });
+  });
+
 });
