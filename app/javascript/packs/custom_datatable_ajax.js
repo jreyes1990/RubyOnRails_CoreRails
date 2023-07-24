89082083346b53d2b4a5266b958deb8efd24d180
jreyes1990 <jarl1990mt@gmail.com>
2023-07-23 23:26:34 -0600

    23-07-2023 23:25 PM, Estructuracion de javascript, busqueda datos por columna en dataTable AJAX.

--- app/javascript/packs/custom_datatable_ajax.js
+++ app/javascript/packs/custom_datatable_ajax.js
@@ -96,52 +96,87 @@ document.addEventListener("turbolinks:load", () => {
     }).click(showPopover).hover(showPopover, hidePopover);
   }
 
-  // Definir la función para agrupar las columnas
-  function groupColumns(api, groupColumn, rows) {
+  // Función para agregar filas de agrupación en la tabla
+  function addGroupRows(api, groupColumn) {
+    var rows = api.rows({ page: 'current' }).nodes();
     var last = null;
     api.column(groupColumn, { page: 'current' }).data().each(function(group, i) {
       if (last !== group) {
-        $(rows).eq(i).before('<tr class="group text-left" style="background: #ffe5a4 !important;"><td colspan="8"><strong>' + group + '</strong></td></tr>');
+        $(rows).eq(i).before('<tr class="group text-left" style="background: #ffe5a4 !important;"><td colspan="12"><strong>' + group + '</strong></td></tr>');
         last = group;
       }
     });
   }
-  
+
+  // Función para deshabilitar las últimas columnas en el DataTable
+  function disableLastColumns(api, numLastColumns) {
+    var totalColumns = api.columns().count(); // Obtenemos el número total de columnas en la tabla.
+    var lastColumnStartIndex = totalColumns - numLastColumns; // Calculamos el índice de inicio de las últimas columnas a deshabilitar.
+
+    api.columns().every(function (index) {
+      var column = this; // Obtenemos la columna actual en la iteración.
+      var isLastColumnToDisable = index >= lastColumnStartIndex; // Verificamos si la columna actual es una de las últimas columnas a deshabilitar.
+
+      if (!isLastColumnToDisable && index !== 0 && index !== 1) {
+        // Creamos el filtro de búsqueda (select) para las columnas habilitadas.
+        var select = $('<select class="select2"><option value="">Busqueda....</option></select>')
+          .appendTo($(column.header())) // Agregamos el filtro al encabezado de la columna.
+          .on('change', function () {
+            // Al cambiar el valor del filtro, realizamos una búsqueda en la columna correspondiente utilizando el método search de la API del DataTable.
+            var val = $.fn.dataTable.util.escapeRegex($(this).val());
+            column.search(val ? '^' + val + '$' : '', true, false).draw();
+          });
+
+        // Evitamos que el filtro y el encabezado reaccionen al clic y, por lo tanto, evitamos el ordenamiento junto con el filtro.
+        $(select).click(function (e) {
+          e.stopPropagation();
+        });
+
+        // Obtenemos los datos únicos de la columna y los ordenamos.
+        var data = column.data().unique().sort();
+
+        // Agregamos las opciones al filtro (select) utilizando la función $.map para crear las etiquetas <option> con los valores únicos de la columna.
+        $(select).append(
+          $.map(data, function (value) {
+            return '<option value="' + value + '">' + value + '</option>';
+          }).join('')
+        );
+
+        // Inicializamos el filtro utilizando el complemento Select2 para mejorar su aspecto y funcionalidad.
+        select.select2();
+      }
+    });
+  }
+
+  function adjustCellContent(node, content, cellWidth) {
+    var tempDiv = $("<div>")
+      .css({
+        position: "absolute",
+        top: -9999, // Colocamos el div fuera del área visible de la página.
+        left: -9999,
+        width: "auto",  // Permite que el contenido determine el ancho del div temporal.
+        whiteSpace: "nowrap", // Evita el salto de línea automático del contenido dentro del div.
+        fontSize: node.style.fontSize,  // Copiamos la fuente y tamaño de la celda original.
+        fontFamily: node.style.fontFamily,
+      })
+      .text(content)  // Asignamos el contenido actual a este div temporal.
+      .appendTo(document.body); // Agregamos el div temporal al cuerpo del documento para que sea visible para JavaScript.
+
+    if (tempDiv.width() > cellWidth) {
+      $(node).html('<div style="white-space:normal;width:' + cellWidth + 'px">' + content + '</div>');
+    }
+
+    // Eliminamos el div temporal, ya que solo se utilizó para medir el ancho del contenido y no es necesario en la página.
+    tempDiv.remove();
+  }
+
   /* DATATABLE AJAX PARA EL MODULO DE FONT_AWESOM */
   var groupColumnAwesome = 4;
-  $("#font-awesome-datatable").DataTable({
+  var fontAwesomeDatatable = $("#font-awesome-datatable").DataTable({
     columnDefs: [{ visible: false, targets: groupColumnAwesome }],
     drawCallback: function (settings) {
-      var api = this.api();
-      var rows = api.rows({ page: 'current' }).nodes();
-      var last = null;
-      /* FUNCION PARA EL USO DE POPOVER */
-      $(function() {
-        $('[title]').attr("data-rel", "tooltip");
-        $("[data-rel='tooltip']").attr("data-placement", "top");
-        var showPopover = function() {
-            $(this).popover('show');
-        };
-        var hidePopover = function() {
-            $(this).popover('hide');
-        };
-        $("[data-rel='tooltip']").popover({
-            trigger: 'manual'
-        }).click(showPopover).hover(showPopover, hidePopover);
-      });
-      /* AGRUPACION DE COLUMNAS */
-      api
-        .column(groupColumnAwesome, { page: 'current' })
-        .data()
-        .each(function (group, i) {
-          if (last !== group) {
-            $(rows)
-                .eq(i)
-                .before('<tr class="group text-left" style="background: #ffe5a4 !important;"><td colspan="8"><strong>' + group + '</strong></td></tr>');
-
-            last = group;
-          }
-        });
+      addGroupRows(this.api(), groupColumnAwesome); // Agrupación de columnas
+      initializePopovers(); // Inicializar Popover después de cargar los datos
     },
     fixedHeader: true,
     stateSave: true,
@@ -160,6 +195,10 @@ document.addEventListener("turbolinks:load", () => {
     },
     buttons: var_buttons,
     pagingType: "full_numbers",
+    autoFill: {
+      enable: true, // Permite el rellenado automático de celdas en la tabla.
+    },
+    initComplete: initializeFontAwesomeDatatable,
     columns: [
       { data: "id", class: "text-center" },
       { data: "icono", class: "text-center" },
@@ -173,41 +212,32 @@ document.addEventListener("turbolinks:load", () => {
     ],
   }).column(4).visible(false);
 
+  // Función que se ejecuta en el evento init.dt para inicializar la tabla.
+  function initializeFontAwesomeDatatable() {
+    var api = this.api();
+    var numLastColumnsToDisable = 6;
+    disableLastColumns(api, numLastColumnsToDisable);
+
+    // Llamamos a la función adjustCellContent para ajustar el contenido de las celdas al cargar o paginar los datos.
+    fontAwesomeDatatable.on("draw.dt", function () {
+      fontAwesomeDatatable.cells(":visible").every(function () {
+        var content = this.data();
+        var cellWidth = $(this.node()).width();
+        adjustCellContent(this.node(), content, cellWidth);
+      });
+    });
+
+    // Inicializamos los Select2 después de deshabilitar las últimas columnas y de dibujar el DataTable.
+    $(api.table().header()).find(".select2-filter").select2();
+  }
+
   /* DATATABLE AJAX PARA EL MODULO DE COLOR */
   var groupColumn = 1;
   $("#codigo-color-datatable").DataTable({
     columnDefs: [{ visible: false, targets: groupColumn }],
     drawCallback: function (settings) {
-      var api = this.api();
-      var rows = api.rows({ page: 'current' }).nodes();
-      var last = null;
-      /* FUNCION PARA EL USO DE POPOVER */
-      $(function() {
-        $('[title]').attr("data-rel", "tooltip");
-        $("[data-rel='tooltip']").attr("data-placement", "top");
-        var showPopover = function() {
-            $(this).popover('show');
-        };
-        var hidePopover = function() {
-            $(this).popover('hide');
-        };
-        $("[data-rel='tooltip']").popover({
-            trigger: 'manual'
-        }).click(showPopover).hover(showPopover, hidePopover);
-      });
-      /* AGRUPACION DE COLUMNAS */
-      api
-        .column(groupColumn, { page: 'current' })
-        .data()
-        .each(function (group, i) {
-          if (last !== group) {
-            $(rows)
-                .eq(i)
-                .before('<tr class="group text-left" style="background: #ffe5a4 !important;"><td colspan="9"><strong>' + group + '</strong></td></tr>');
-
-            last = group;
-          }
-        });
+      addGroupRows(this.api(), groupColumn);  // Agrupación de columnas
+      initializePopovers(); // Inicializar Popover después de cargar los datos
     },
     fixedHeader: true,
     stateSave: true,
@@ -244,36 +274,8 @@ document.addEventListener("turbolinks:load", () => {
   $("#area-empresa-datatable").DataTable({
     columnDefs: [{ visible: false, targets: groupColumn }],
     drawCallback: function (settings) {
-      var api = this.api();
-      var rows = api.rows({ page: 'current' }).nodes();
-      var last = null;
-      /* FUNCION PARA EL USO DE POPOVER */
-      $(function() {
-        $('[title]').attr("data-rel", "tooltip");
-        $("[data-rel='tooltip']").attr("data-placement", "top");
-        var showPopover = function() {
-            $(this).popover('show');
-        };
-        var hidePopover = function() {
-            $(this).popover('hide');
-        };
-        $("[data-rel='tooltip']").popover({
-            trigger: 'manual'
-        }).click(showPopover).hover(showPopover, hidePopover);
-      });
-      /* AGRUPACION DE COLUMNAS */
-      api
-        .column(groupColumn, { page: 'current' })
-        .data()
-        .each(function (group, i) {
-          if (last !== group) {
-            $(rows)
-                .eq(i)
-                .before('<tr class="group text-left" style="background: #ffe5a4 !important;"><td colspan="8"><strong>' + group + '</strong></td></tr>');
-
-            last = group;
-          }
-        });
+      addGroupRows(this.api(), groupColumn);  // Agrupación de columnas
+      initializePopovers(); // Inicializar Popover después de cargar los datos
     },
     fixedHeader: true,
     stateSave: true,
@@ -308,36 +310,8 @@ document.addEventListener("turbolinks:load", () => {
   $("#personas-area-datatable").DataTable({
     columnDefs: [{ visible: false, targets: groupColumn }],
     drawCallback: function (settings) {
-      var api = this.api();
-      var rows = api.rows({ page: 'current' }).nodes();
-      var last = null;
-      /* FUNCION PARA EL USO DE POPOVER */
-      $(function() {
-        $('[title]').attr("data-rel", "tooltip");
-        $("[data-rel='tooltip']").attr("data-placement", "top");
-        var showPopover = function() {
-            $(this).popover('show');
-        };
-        var hidePopover = function() {
-            $(this).popover('hide');
-        };
-        $("[data-rel='tooltip']").popover({
-            trigger: 'manual'
-        }).click(showPopover).hover(showPopover, hidePopover);
-      });
-      /* AGRUPACION DE COLUMNAS */
-      api
-        .column(groupColumn, { page: 'current' })
-        .data()
-        .each(function (group, i) {
-          if (last !== group) {
-            $(rows)
-                .eq(i)
-                .before('<tr class="group text-left" style="background: #ffe5a4 !important;"><td colspan="8"><strong>' + group + '</strong></td></tr>');
-
-            last = group;
-          }
-        });
+      addGroupRows(this.api(), groupColumn);  // Agrupación de columnas
+      initializePopovers(); // Inicializar Popover después de cargar los datos
     },
     fixedHeader: true,
     stateSave: true,
@@ -373,36 +347,8 @@ document.addEventListener("turbolinks:load", () => {
   $("#opcion-datatable").DataTable({
     columnDefs: [{ visible: false, targets: groupColumn }],
     drawCallback: function (settings) {
-      var api = this.api();
-      var rows = api.rows({ page: 'current' }).nodes();
-      var last = null;
-      /* FUNCION PARA EL USO DE POPOVER */
-      $(function() {
-        $('[title]').attr("data-rel", "tooltip");
-        $("[data-rel='tooltip']").attr("data-placement", "top");
-        var showPopover = function() {
-            $(this).popover('show');
-        };
-        var hidePopover = function() {
-            $(this).popover('hide');
-        };
-        $("[data-rel='tooltip']").popover({
-            trigger: 'manual'
-        }).click(showPopover).hover(showPopover, hidePopover);
-      });
-      /* AGRUPACION DE COLUMNAS */
-      api
-          .column(groupColumn, { page: 'current' })
-          .data()
-          .each(function (group, i) {
-            if (last !== group) {
-              $(rows)
-                  .eq(i)
-                  .before('<tr class="group text-left" style="background: #ffe5a4 !important;"><td colspan="10"><strong>' + group + '</strong></td></tr>');
-
-              last = group;
-            }
-          });
+      addGroupRows(this.api(), groupColumn);  // Agrupación de columnas
+      initializePopovers(); // Inicializar Popover después de cargar los datos
     },
     fixedHeader: true,
     stateSave: true,
@@ -439,36 +385,8 @@ document.addEventListener("turbolinks:load", () => {
   $("#menu-rol-datatable").DataTable({
     columnDefs: [{ visible: false, targets: groupColumn }],
     drawCallback: function (settings) {
-      var api = this.api();
-      var rows = api.rows({ page: 'current' }).nodes();
-      var last = null;
-      /* FUNCION PARA EL USO DE POPOVER */
-      $(function() {
-        $('[title]').attr("data-rel", "tooltip");
-        $("[data-rel='tooltip']").attr("data-placement", "top");
-        var showPopover = function() {
-            $(this).popover('show');
-        };
-        var hidePopover = function() {
-            $(this).popover('hide');
-        };
-        $("[data-rel='tooltip']").popover({
-            trigger: 'manual'
-        }).click(showPopover).hover(showPopover, hidePopover);
-      });
-      /* AGRUPACION DE COLUMNAS */
-      api
-        .column(groupColumn, { page: 'current' })
-        .data()
-        .each(function (group, i) {
-          if (last !== group) {
-            $(rows)
-                .eq(i)
-                .before('<tr class="group text-left" style="background: #ffe5a4 !important;"><td colspan="10"><strong>' + group + '</strong></td></tr>');
-
-            last = group;
-          }
-        });
+      addGroupRows(this.api(), groupColumn);  // Agrupación de columnas
+      initializePopovers(); // Inicializar Popover después de cargar los datos
     },
     fixedHeader: true,
     stateSave: true,
@@ -501,20 +419,8 @@ document.addEventListener("turbolinks:load", () => {
   /* DATATABLE AJAX PARA EL MODULO DE COMPONENTE */
   $("#componente-datatable").DataTable({
     drawCallback: function (settings) {
-      /* FUNCION PARA EL USO DE POPOVER */
-      $(function() {
-        $('[title]').attr("data-rel", "tooltip");
-        $("[data-rel='tooltip']").attr("data-placement", "top");
-        var showPopover = function() {
-            $(this).popover('show');
-        };
-        var hidePopover = function() {
-            $(this).popover('hide');
-        };
-        $("[data-rel='tooltip']").popover({
-            trigger: 'manual'
-        }).click(showPopover).hover(showPopover, hidePopover);
-      });
+      // Inicializar Popover después de cargar los datos
+      initializePopovers();
     },
     fixedHeader: true,
     stateSave: true,
@@ -547,36 +453,8 @@ document.addEventListener("turbolinks:load", () => {
   $("#opcion-ca-datatable").DataTable({
     columnDefs: [{ visible: false, targets: groupColumn }],
     drawCallback: function (settings) {
-      var api = this.api();
-      var rows = api.rows({ page: 'current' }).nodes();
-      var last = null;
-      /* FUNCION PARA EL USO DE POPOVER */
-      $(function() {
-        $('[title]').attr("data-rel", "tooltip");
-        $("[data-rel='tooltip']").attr("data-placement", "top");
-        var showPopover = function() {
-            $(this).popover('show');
-        };
-        var hidePopover = function() {
-            $(this).popover('hide');
-        };
-        $("[data-rel='tooltip']").popover({
-            trigger: 'manual'
-        }).click(showPopover).hover(showPopover, hidePopover);
-      });
-      /* AGRUPACION DE COLUMNAS */
-      api
-        .column(groupColumn, { page: 'current' })
-        .data()
-        .each(function (group, i) {
-          if (last !== group) {
-            $(rows)
-                .eq(i)
-                .before('<tr class="group text-left" style="background: #ffe5a4 !important;"><td colspan="8"><strong>' + group + '</strong></td></tr>');
-
-            last = group;
-          }
-        });
+      addGroupRows(this.api(), groupColumn);  // Agrupación de columnas
+      initializePopovers(); // Inicializar Popover después de cargar los datos
     },
     fixedHeader: true,
     stateSave: true,
