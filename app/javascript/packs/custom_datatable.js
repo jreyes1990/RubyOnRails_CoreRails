eeab080e97e0686e77c84dcfb91aa6abebff76e1
jreyes1990 <jarl1990mt@gmail.com>
2023-08-13 04:24:27 -0600

    13-08-2023 04:20 PM, Reestructuracion de codigo JavaScript dataTable.

--- app/javascript/packs/custom_datatable.js
+++ app/javascript/packs/custom_datatable.js
@@ -29,7 +29,7 @@ document.addEventListener("turbolinks:load", () => {
     }
   };
 
-  var var_datatable =
+  var var_datatable = 
     "" +
     "<'row'<'col-sm-3'l><'col-sm-6 text-center'B><'col-sm-3'f>>" +
     "<'row'<'col-sm-12'tr>>" +
@@ -144,126 +144,197 @@ document.addEventListener("turbolinks:load", () => {
    * Fin de Configuracion para dataTables
    * *********************************************************/
   
-  // Definimos la función disableLastColumns que deshabilitará las últimas columnas
-  // y dejará la primera columna intacta.
-  function disableLastColumns(api, numLastColumns) {
-    var totalColumns = api.columns().count(); // Obtenemos el número total de columnas en la tabla.
-    var lastColumnStartIndex = totalColumns - numLastColumns; // Calculamos el índice de inicio de las últimas columnas a deshabilitar.
-
-    // Recorremos todas las columnas utilizando el método every de la API del DataTable.
+  function searchColumnsSelect(api, searchableColumns) {
     api.columns().every(function (index) {
-      var column = this;  // Obtenemos la columna actual en la iteración.
-      var isLastColumnToDisable = index >= lastColumnStartIndex;  // Verificamos si la columna actual es una de las últimas columnas a deshabilitar.
+      var column = this;
+      var isSearchable = searchableColumns.includes(index);
 
-      if (!isLastColumnToDisable && index !== 0) {  // Comprobamos que la columna no es una de las últimas columnas a deshabilitar (index !== 0 evita la primera columna).
-        // Creamos el filtro de búsqueda (select) para las columnas habilitadas.
+      if (isSearchable) {
         var select = $('<select class="select2"><option value="">Busqueda....</option></select>')
-          .appendTo($(column.header())) // Agregamos el filtro al encabezado de la columna.
+          .appendTo($(column.header()))
           .on('change', function () {
-            // Al cambiar el valor del filtro, realizamos una búsqueda en la columna correspondiente utilizando el método search de la API del DataTable.
             var val = $.fn.dataTable.util.escapeRegex($(this).val());
             column.search(val ? '^' + val + '$' : '', true, false).draw();
           });
 
-        // Evitamos que el filtro y el encabezado reaccionen al clic y, por lo tanto, evitamos el ordenamiento junto con el filtro.
         $(select).click(function (e) {
           e.stopPropagation();
         });
 
-        // Obtenemos los datos únicos de la columna y los ordenamos.
         var data = column.data().unique().sort();
 
-        // Agregamos las opciones al filtro (select) utilizando la función $.map para crear las etiquetas <option> con los valores únicos de la columna.
         $(select).append(
           $.map(data, function (value) {
             return '<option value="' + value + '">' + value + '</option>';
           }).join('')
         );
 
-        // Inicializamos el filtro utilizando el complemento Select2 para mejorar su aspecto y funcionalidad.
         select.select2();
-
-        // Ajustamos el ancho del filtro (select) basado en el ancho del texto de la descripción de la columna (headerText).
-        // Esto se hace para que el filtro tenga el mismo ancho que el encabezado de la columna.
-        var headerText = $(column.header()).text();
         var headerWidth = $(column.header()).width();
         $(select).css('width', headerWidth);
       }
     });
   }
+  
+  //configuracion para datatables agrupados por columnas dinamico
+  /**
+  * Función para agrupar filas en un DataTable en base a múltiples columnas.
+  * @param {string} var_dom - Selector o elemento DOM donde se mostrará el DataTable. Por ejemplo, '#datatable_familia_producto' o document.getElementById('datatable_familia_producto').
+  * @param {Array} agrupaciones - Un array que contiene objetos con detalles de las columnas que se utilizarán para la agrupación. Cada objeto debe tener la forma { columnIndex: number, label: string }, donde columnIndex es el índice de la columna (comenzando desde 0) y label es el nombre o etiqueta de la columna que se utilizará para mostrar el nombre del grupo.
+  * @param {Array|null} columnDefs - Un array opcional que contiene configuraciones adicionales para las columnas del DataTable, en el formato requerido por DataTables columnDefs. Puede ser nulo si no se necesita ninguna configuración adicional.
+  * @param {Object} options - Opciones adicionales para configurar el DataTable, siguiendo el formato de configuración del DataTable. Puedes personalizar opciones como el orden, dom, lenguaje, botones, etc.
+  * @param {Array} searchColumns - Un array con los índices de las columnas en las que se aplicará la búsqueda selectiva.
+  * @param {string} tipoPagina - Tipo de paginación para el DataTable.
+  */
+  function agrupar_filas(var_dom, agrupaciones, columnDefs, options, searchColumns, tipoPagina) {
+    // Comprueba si agrupaciones está vacío
+    if (!agrupaciones || agrupaciones.length === 0) {
+      // Aquí puedes inicializar la tabla sin el rowGroup, si lo deseas
+      $(var_dom).DataTable({
+        "columnDefs": columnDefs || [],
+        fixedHeader: options.fixedHeader,
+        stateSave: options.stateSave,
+        stateDuration: options.stateDuration,
+        dom: options.dom,
+        language: options.language,
+        responsive: options.responsive,
+        lengthChange: options.lengthChange,
+        lengthMenu: options.lengthMenu,
+        select: options.select,
+        buttons: options.buttons,
+        initComplete: function () {
+          var api = this.api();
+          searchColumnsSelect(api, searchColumns); // Llamada a la función para aplicar búsqueda selectiva en las columnas especificadas
+        },
+        pagingType: tipoPagina
+      });
 
-  // La función adjustCellContent ajusta el contenido de la celda para que quepa dentro del ancho de la columna.
-  // Recibe tres parámetros:
-  // 1. node: El nodo HTML de la celda que se va a ajustar.
-  // 2. content: El contenido actual de la celda.
-  // 3. cellWidth: El ancho de la columna donde se encuentra la celda.
-  function adjustCellContent(node, content, cellWidth) {
-    // Creamos un elemento div temporal para medir el ancho del contenido de la celda sin que sea visible en la página.
-    // Establecemos algunas propiedades CSS para que el div sea invisible y no afecte el diseño.
-    var tempDiv = $("<div>")
-      .css({
-        position: "absolute",
-        top: -9999, // Colocamos el div fuera del área visible de la página.
-        left: -9999,
-        width: "auto",  // Permite que el contenido determine el ancho del div temporal.
-        whiteSpace: "nowrap", // Evita el salto de línea automático del contenido dentro del div.
-        fontSize: node.style.fontSize,  // Copiamos la fuente y tamaño de la celda original.
-        fontFamily: node.style.fontFamily,
-      })
-      .text(content)  // Asignamos el contenido actual a este div temporal.
-      .appendTo(document.body); // Agregamos el div temporal al cuerpo del documento para que sea visible para JavaScript.
-
-    // Comparamos el ancho del contenido dentro del div temporal con el ancho de la celda.
-    // Si el ancho del contenido es mayor que el ancho de la celda, significa que no cabe completamente.
-    if (tempDiv.width() > cellWidth) {
-      // Si el contenido no cabe, ajustamos el contenido de la celda para que se muestre correctamente sin exceder el ancho de la columna.
-      // Utilizamos la función html() de jQuery para establecer el contenido de la celda.
-      // Agregamos un div adicional dentro de la celda con un estilo que permita el salto de línea para mostrar el contenido completo.
-      // Limitamos el ancho del div adicional al ancho de la columna para evitar que se desborde.
-      $(node).html('<div style="white-space:normal;width:' + cellWidth + 'px">' + content + '</div>');
+      return;
     }
 
-    // Eliminamos el div temporal, ya que solo se utilizó para medir el ancho del contenido y no es necesario en la página.
-    tempDiv.remove();
+    var dataSrc = agrupaciones.map(agrupacion => agrupacion.columnIndex);
+    var orden = agrupaciones.length > 1 ? agrupaciones.map(agrupacion => [agrupacion.columnIndex, agrupacion.order || 'asc']) : [];
+
+    $(var_dom).DataTable({
+      "order": orden,
+      "columnDefs": [...agrupaciones.map(agrupacion => ({ "visible": false, "targets": agrupacion.columnIndex })), ...(columnDefs ?? [])],
+      rowGroup: {
+        dataSrc: dataSrc,
+        startRender: function (rows, group, level) {
+          var className = '';
+          var groupName = '';
+
+          agrupaciones.forEach((agrupacion, index) => {
+            if (level === index) {
+              className = `group-start-level-${index}`;
+              groupName = `${agrupacion.label}: ${group}`;
+            }
+          });
+
+          return $('<tr/>').append(`<td colspan="20"> ${groupName} </td>`).addClass(className);
+        }
+      },
+      fixedHeader: options.fixedHeader,
+      stateSave: options.stateSave,
+      stateDuration: options.stateDuration,
+      dom: options.dom,
+      language: options.language,
+      responsive: options.responsive,
+      lengthChange: options.lengthChange,
+      lengthMenu: options.lengthMenu,
+      select: options.select,
+      buttons: options.buttons,
+      initComplete: function () {
+        var api = this.api();
+        searchColumnsSelect(api, searchColumns);
+      },
+      pagingType: tipoPagina
+    });
   }
 
-  /* Inicio del uso de dataTable */
-  // Seleccionamos el elemento con el id "datatable" y lo convertimos en un DataTable.
-  var table = $("#datatable").DataTable({
-    fixedHeader: true,    // Encabezado fijo al hacer scroll.
-    stateSave: true,      // Guarda el estado actual de la tabla para recordar las configuraciones personalizadas del usuario.
-    stateDuration: 1200,  // Tiempo en milisegundos que se guarda el estado de la tabla en el almacenamiento local.
-    responsive: true,     // Permite que la tabla se adapte y sea responsive en diferentes dispositivos.
-    dom: var_datatable,   // Define la disposición personalizada de los elementos del DataTable (pueden incluir botones, filtros, etc.).
-    language: espaniol,   // Configura el idioma de la tabla, en este caso, español.
-    lengthChange: true,   // Permite al usuario cambiar la cantidad de registros mostrados por página.
-    select: true,         // Permite la selección de filas en la tabla.
+  //opciones que se pueden utilizar en el datatable - estas son estandar
+  var opciones = {
+    fixedHeader: true,            // Encabezado fijo al hacer scroll.
+    stateSave: true,              // Guarda el estado actual de la tabla para recordar las configuraciones personalizadas del usuario.
+    stateDuration: 1200,          // Tiempo en milisegundos que se guarda el estado de la tabla en el almacenamiento local.
+    dom: var_datatable,           // Define la disposición personalizada de los elementos del DataTable (pueden incluir botones, filtros, etc.).
+    language: espaniol,           // Configura el idioma de la tabla, en este caso, español.
+    responsive: true,             // Permite que la tabla se adapte y sea responsive en diferentes dispositivos.
+    lengthChange: true,           // Permite al usuario cambiar la cantidad de registros mostrados por página.
     lengthMenu: [
       // Define las opciones de cantidad de registros por página en el menú desplegable.
       [5, 10, 15, 20, 25, 50, -1],      // Opciones de cantidad de registros.
       [5, 10, 15, 20, 25, 50, 'Todos'], // Etiquetas que se muestran en el menú desplegable.
     ],
-    buttons: button_datatable,  // Define los botones personalizados que se agregarán a la tabla.
-    pagingType: "full_numbers", // Tipo de paginación, muestra todos los controles disponibles (primero, último, siguiente, anterior, etc.).
-    autoFill: {
-      enable: true, // Permite el rellenado automático de celdas en la tabla.
-    },
-    initComplete: function () {
-      // Función de inicialización que se ejecuta al completar la creación del DataTable.
-      var api = this.api(); // Obtenemos el objeto API del DataTable para interactuar con él.
-      var numLastColumnsToDisable = 4; // Número de últimas columnas a deshabilitar.
-      disableLastColumns(api, numLastColumnsToDisable); // Llamamos a la función disableLastColumns para deshabilitar las últimas columnas.
-    },
-  });
+    select: true,                 // Permite la selección de filas en la tabla.
+    buttons: button_datatable,    // Define los botones personalizados que se agregarán a la tabla.
+    initComplete: true,
+    pagingType: true              // Tipo de paginación, muestra todos los controles disponibles (primero, último, siguiente, anterior, etc.).
+  };
 
-  // Evento que se ejecuta cuando se dibuja el DataTable (al cargar o paginar los datos).
-  table.on("draw.dt", function () {
-    // Seleccionamos todas las celdas visibles en la tabla y recorremos cada celda.
-    table.cells(":visible").every(function () {
-      var content = this.data();  // Obtenemos el contenido de la celda actual.
-      var cellWidth = $(this.node()).width(); // Obtenemos el ancho de la columna donde se encuentra la celda.
-      adjustCellContent(this.node(), content, cellWidth); // Llamamos a la función adjustCellContent para ajustar el contenido de la celda.
-    });
-  });
+  agrupar_filas(
+    '#datatable_usuario',
+    [],
+    [
+      { responsivePriority: 1, targets: [0, 1, -1] },
+      { responsivePriority: 2, targets: [2, -2] },
+      { responsivePriority: 3, targets: [3, 4] }
+    ],
+    opciones,
+    [1, 2],
+    "full_numbers"
+  )
+
+  agrupar_filas(
+    '#datatable_empresa',
+    [],
+    [
+      { responsivePriority: 1, targets: [1, -1, -2] },
+      { responsivePriority: 2, targets: [2, -3] },
+      { responsivePriority: 3, targets: [0, -4] }
+    ],
+    opciones,
+    [1, 2],
+    "full_numbers"
+  )
+
+  agrupar_filas(
+    '#datatable_rol',
+    [],
+    [
+      { responsivePriority: 1, targets: [1, -1, -2] },
+      { responsivePriority: 2, targets: [0, -3] },
+      { responsivePriority: 3, targets: [2] }
+    ],
+    opciones,
+    [1],
+    "full_numbers"
+  )
+
+  agrupar_filas(
+    '#datatable_menu',
+    [],
+    [
+      { responsivePriority: 1, targets: [2, -1, -2] },
+      { responsivePriority: 2, targets: [1, -3] },
+      { responsivePriority: 3, targets: [0, 3] }
+    ],
+    opciones,
+    [2],
+    "full_numbers"
+  )
+
+  agrupar_filas(
+    '#datatable_atributo',
+    [],
+    [
+      { responsivePriority: 1, targets: [1, -1, -2] },
+      { responsivePriority: 2, targets: [0, -3] },
+      { responsivePriority: 3, targets: [2] }
+    ],
+    opciones,
+    [1],
+    "full_numbers"
+  )
 
 });
